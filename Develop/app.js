9a20df04dda61a99b072d1d870088a368779e063
Annema Cozmoz <mikewittorp@gmail.com>
2020-11-09 14:49:26 -0500

    Fixed flex to d-md-flex which helped the cards stack appropriately on mobile

--- Develop/app.js
+++ Develop/app.js
@@ -38,23 +38,63 @@ inquirer
         },
         {
             type: "input",
-            message: "What is your engineer's name?",
-            name: "engineerName"
+            message: "What is your 1st engineer's name?",
+            name: "engineer1Name"
         },
         {
             type: "input",
-            message: "What is your engineer's id?",
-            name: "engineerId"
+            message: "What is your 1st engineer's id?",
+            name: "engineer1Id"
         },
         {
             type: "input",
-            message: "What is your engineer's email?",
-            name: "engineerEmail"
+            message: "What is your 1st engineer's email?",
+            name: "engineer1Email"
         },
         {
             type: "input",
-            message: "What is your engineer's github username?",
-            name: "engineerGithub"
+            message: "What is your 1st engineer's github username?",
+            name: "engineer1Github"
+        },
+        {
+            type: "input",
+            message: "What is your 2nd engineer's name?",
+            name: "engineer2Name"
+        },
+        {
+            type: "input",
+            message: "What is your 2nd engineer's id?",
+            name: "engineer2Id"
+        },
+        {
+            type: "input",
+            message: "What is your 2nd engineer's email?",
+            name: "engineer2Email"
+        },
+        {
+            type: "input",
+            message: "What is your 2nd engineer's github username?",
+            name: "engineer2Github"
+        },
+        {
+            type: "input",
+            message: "What is your 3rd engineer's name?",
+            name: "engineer3Name"
+        },
+        {
+            type: "input",
+            message: "What is your 3rd engineer's id?",
+            name: "engineer3Id"
+        },
+        {
+            type: "input",
+            message: "What is your 3rd engineer's email?",
+            name: "engineer3Email"
+        },
+        {
+            type: "input",
+            message: "What is your 3rd engineer's github username?",
+            name: "engineer3Github"
         },
         {
             type: "input",
@@ -93,7 +133,7 @@ inquirer
     <title>My Team</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../Assets/style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
@@ -101,7 +141,7 @@ inquirer
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+                <h1 class="text-center">Terbo Team</h1>
             </div>
         </div>
     </div>
@@ -125,15 +165,45 @@ inquirer
 
 <div class="card employee-card">
     <div class="card-header">
-        <h2 class="card-title" id="engineer-name">${response.engineerName}</h2>
+        <h2 class="card-title" id="engineer-name">${response.engineer1Name}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
-            <li class="list-group-item" id="engineer-id">ID: ${response.engineerId}</li>
-            <li class="list-group-item" id="engineer-email">Email: <a href="mailto:${response.engineerEmail}">${response.engineerEmail}</a></li>
-            <li class="list-group-item" id="engineer-github">GitHub: <a href="https://github.com/${response.engineerGithub}"
-                    target="_blank" rel="noopener noreferrer">${response.engineerGithub}</a></li>
+            <li class="list-group-item" id="engineer-id">ID: ${response.engineer1Id}</li>
+            <li class="list-group-item" id="engineer-email">Email: <a href="mailto:${response.engineer1Email}">${response.engineer1Email}</a></li>
+            <li class="list-group-item" id="engineer-github">GitHub: <a href="https://github.com/${response.engineer1Github}"
+                    target="_blank" rel="noopener noreferrer">${response.engineer1Github}</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title" id="engineer-name">${response.engineer2Name}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item" id="engineer-id">ID: ${response.engineer2Id}</li>
+            <li class="list-group-item" id="engineer-email">Email: <a href="mailto:${response.engineer2Email}">${response.engineer2Email}</a></li>
+            <li class="list-group-item" id="engineer-github">GitHub: <a href="https://github.com/${response.engineer2Github}"
+                    target="_blank" rel="noopener noreferrer">${response.engineer2Github}</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title" id="engineer-name">${response.engineer3Name}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item" id="engineer-id">ID: ${response.engineer3Id}</li>
+            <li class="list-group-item" id="engineer-email">Email: <a href="mailto:${response.engineer3Email}">${response.engineer3Email}</a></li>
+            <li class="list-group-item" id="engineer-github">GitHub: <a href="https://github.com/${response.engineer3Github}"
+                    target="_blank" rel="noopener noreferrer">${response.engineer3Github}</a></li>
         </ul>
     </div>
 </div>
@@ -156,12 +226,7 @@ inquirer
     </div>
 </body>
 
-</html>
-
-
-
-
-        `
+</html>`
 
         fs.writeFile('team.html', renderHtml, err => {
             if (err) {
